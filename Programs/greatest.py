60c2a814755a082b9a8daf15413fc8b6286bf282
Tandy ! <ustanay4@gmail.com>
2020-08-17 11:39:22 +0530

    my commit

--- Programs/greatest.py
+++ Programs/greatest.py
@@ -1,17 +1,3 @@
-
-# def even_fibonacci_sum(n):
-#     n1 = 1 #Fn-2
-#     n2 = 1 #Fn-1
-#     sum = 0
-#     while True :
-#         t = n1 + n2 
-#         if t >= n: return sum
-#         if t % 2 == 0: sum += t
-#         n2= n1
-#         n1 = t
-
-
-
 a = int(input(""))
 b = int(input(""))
 c = int(input(""))
---
+++ largestprimefator.py
@@ -0,0 +1,4 @@
+num = int(input("enter a number: "))
+for i in range (num+1):
+    if num % i == 0:
+        print(i)
