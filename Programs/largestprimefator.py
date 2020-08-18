42cb9940eb29566bc7fafcb4c11ebd1bc28a9f1c
Tandy ! <ustanay4@gmail.com>
2020-08-18 13:37:54 +0530

    my commit

--- Programs/largestprimefator.py
+++
@@ -1,8 +0,0 @@
-num = int(input("enter a number: "))
-for i in range (1,num+1):
-    if num % i == 0:
-        # print(i)
-        for j in range(2, i):
-            if i%j != 0:
-                continue
-            if i%j == 
