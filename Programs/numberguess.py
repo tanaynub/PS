5df3c2edfd671ec34a99c306d785878fc91c3f1a
Tandy ! <ustanay4@gmail.com>
2020-08-18 08:46:03 +0530

    my commit

--- Programs/numberguess.py
+++ Programs/numberguess.py
@@ -1,3 +1,16 @@
+a = 1000
+
+
+def input:
+    a = input("y or n")
+    if a == "y":
+        global res = true
+    if a == "n":
+        global res = false
+
+
+
+
 def guess:
     while true:
         
