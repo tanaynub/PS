18969175d7f0d577d70e3f5e3b48bf2455595d17
Tandy ! <ustanay4@gmail.com>
2020-08-20 10:15:16 +0530

    my commi8t

--- Programs/try.py
+++ Programs/try.py
@@ -1,23 +1,2 @@
-#types of variables:
-#string
-# integer
-# floats
-# boolean
-
-
-
-name = 'tanay'
-age = 15
-height = 5.7
-isDumb = True
-
-
-name = "Tanay"
-remarks = '''He is a very good boy,
-he can sing
-he can dance'''
-
-
-
-
-
+for i in range(51):
+    print('heyy')
