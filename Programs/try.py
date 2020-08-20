fdf01965a76ffd1e5c61a359a649d0b4147955ad
Tandy ! <ustanay4@gmail.com>
2020-08-20 10:12:46 +0530

    my commit

---
+++ Programs/try.py
@@ -0,0 +1,23 @@
+#types of variables:
+#string
+# integer
+# floats
+# boolean
+
+
+
+name = 'tanay'
+age = 15
+height = 5.7
+isDumb = True
+
+
+name = "Tanay"
+remarks = '''He is a very good boy,
+he can sing
+he can dance'''
+
+
+
+
+
